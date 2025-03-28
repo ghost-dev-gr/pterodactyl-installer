050e6626421903f37d41a689235fb355fe5c14e0
ZeroBot Founder:Naoum <78745524+ghost-dev-gr@users.noreply.github.com>
2025-03-28 23:54:41 +0200

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -50,7 +50,7 @@ export SUPPORTED=false
 
 # download URLs
 export PANEL_DL_URL="https://github.com/ghost-dev-gr/panel/releases/latest/download/panel.tar.gz"
-export WINGS_DL_BASE_URL="https://github.com/ghost-dev-gr/wings/releases/latest/download/wings.tar.gz"
+export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
 export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/dev-ghost-gr/pterodactyl-installer"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
