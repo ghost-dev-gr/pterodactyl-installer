c7fa7d5b5b51420e32650e8136094b18c6e3f95c
ZeroBot Founder:Naoum <78745524+ghost-dev-gr@users.noreply.github.com>
2025-03-30 20:42:15 +0300

    Update wings.sh

--- installers/wings.sh
+++ installers/wings.sh
@@ -353,8 +353,7 @@ perform_install() {
 # Add proxy routes file before installing Go
 echo "=> Checking for router_server_proxy.go..."
 echo "=> Current working directory: $(pwd)"
-cd /root
-cd pterodactyl-installer/installers
+cd /root/pterodactyl-installer/installers
 if [ -f "router_server_proxy.go" ]; then
   output "Adding custom proxy routes..."
 
