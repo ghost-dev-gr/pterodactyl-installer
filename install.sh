bda710c696e71c604b757743d39837d7ce0a3a2b
ZeroBot Founder:Naoum <78745524+ghost-dev-gr@users.noreply.github.com>
2025-04-08 21:50:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,37 @@
 
 set -e
 
+######################################################################################
+#                                                                                    #
+# Project 'pterodactyl-installer'                                                    #
+#                                                                                    #
+# Copyright (C) 2018 - 2025, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
+#                                                                                    #
+#   This program is free software: you can redistribute it and/or modify             #
+#   it under the terms of the GNU General Public License as published by             #
+#   the Free Software Foundation, either version 3 of the License, or                #
+#   (at your option) any later version.                                              #
+#                                                                                    #
+#   This program is distributed in the hope that it will be useful,                  #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
+#   GNU General Public License for more details.                                     #
+#                                                                                    #
+#   You should have received a copy of the GNU General Public License                #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
+#                                                                                    #
+# https://github.com/pterodactyl-installer/pterodactyl-installer/blob/master/LICENSE #
+#                                                                                    #
+# This script is not associated with the official Pterodactyl Project.               #
+# https://github.com/ghost-dev-gr/pterodactyl-installer                     #
+#                                                                                    #
+######################################################################################
 
 export GITHUB_SOURCE="v1.1.1"
 export SCRIPT_RELEASE="v1.1.1"
 export GITHUB_BASE_URL="https://raw.githubusercontent.com/ghost-dev-gr/pterodactyl-installer"
 
-EXPORT_LOGS="/var/log/pterodactyl-installer.log"
+LOG_PATH="/var/log/pterodactyl-installer.log"
 
 # check for curl
 if ! [ -x "$(command -v curl)" ]; then
@@ -22,28 +47,26 @@ curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
 # shellcheck source=lib/lib.sh
 source /tmp/lib.sh
 
-perfrom() {
-  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$EXPORT_LOGS="/var/log/pterodactyl-installer.log"
-
+execute() {
+  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
 
   [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
   update_lib_source
-  start_gui "${1//_canary/}" |& tee -a $EXPORT_LOGS="/var/log/pterodactyl-installer.log"
-
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
 
   if [[ -n $2 ]]; then
     echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
     read -r CONFIRM
     if [[ "$CONFIRM" =~ [Yy] ]]; then
-      perform "$2"
+      execute "$2"
     else
-      slip "Installation of $2 aborted."
+      error "Installation of $2 aborted."
       exit 1
     fi
   fi
 }
 
-handshake ""
+welcome ""
 
 done=false
 while [ "$done" == false ]; do
@@ -71,19 +94,19 @@ while [ "$done" == false ]; do
     "uninstall_canary"
   )
 
-  result "What would you like to do?"
+  output "What would you like to do?"
 
   for i in "${!options[@]}"; do
-    result "[$i] ${options[$i]}"
+    output "[$i] ${options[$i]}"
   done
 
   echo -n "* Input 0-$((${#actions[@]} - 1)): "
   read -r action
 
-  [ -z "$action" ] && slip "Input is required" && continue
+  [ -z "$action" ] && error "Input is required" && continue
 
   valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && slip "Invalid option"
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
   [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
 done
 
