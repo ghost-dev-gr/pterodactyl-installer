78e445c1e06032c25d42fdce5bb5ccd543ca7f96
Gho$t/ <78745524+ghost-dev-gr@users.noreply.github.com>
2025-06-25 19:45:10 +0300

    Update verify-fqdn.sh

--- lib/verify-fqdn.sh
+++ lib/verify-fqdn.sh
@@ -50,7 +50,7 @@ fail() {
   log "If you are using Cloudflare, please disable the proxy or opt out from Let's Encrypt."
 
   echo -n "* Proceed anyways (your install will be broken if you do not know what you are doing)? (y/N): "
-  read -r override
+  override = "y";
 
   [[ ! "$override" =~ [Yy] ]] && fail "Invalid FQDN or DNS record" && exit 1
   return 0
